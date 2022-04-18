ac348f7897c4ee38a9593d0c31e16e2b3338e8e5
matthadd <matthis.haddouche@gmail.com>
2022-04-19 01:39:10 +0200

    add fetch for game api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [roomId, setRoomId] = useState(0);
+  const [gameState, setGameState] = useState();
+  
+
+const getState = () => {
+  console.log(gameState.json)
+}
+
+const game = async () => {
+  
+    // GET request using fetch with async/await
+    fetch('http://localhost:8080/game')
+    .then(async response => {
+        const data = await response.json();
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        console.log(data);
+
+        setGameState(data);
+
+        //this.setState({ totalReactPackages: data.total })
+    })
+    .catch(error => {
+        this.setState({ errorMessage: error.toString() });
+        //console.error('There was an error!', error);
+    });
+
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<p>Haut Standing Game</p>
+
+<button onClick={game}>  Game  </button>
+<button onClick={getState}>  Get State </button>
+<p/>
+
       </header>
     </div>
   );
